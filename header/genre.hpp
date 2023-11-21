dfa78f3f8a6e5d2775c2fd453cb9a79295ce8334
Xekuu <tommynguyen778@gmail.com>
2023-11-21 13:41:07 -0800

    enumeration class initialization

---
+++ header/genre.hpp
@@ -0,0 +1,19 @@
+#ifndef GENRE_HPP
+#define GENRE_HPP
+
+enum class Genre : unint8_t {
+    DRAMA,
+    COMEDY,
+    ACTION,
+    ROMANCE,
+    SCIFI,
+    MYSTERY,
+    HORROR,
+    ADVENTURE,
+    ANIMATION,
+    HISTORY,
+    MUSIC,
+    CRIME
+};
+
+#endif /* GENRE_HPP */
