21c87cc5817fa64b0bcd124202c9ab372f34118a
Alexander Back <aback005@xe-01.cs.ucr.edu>
2023-11-21 13:10:53 -0800

    added classes to create a trending movie list and to print them

---
+++ header/printTrendingMovies.h
@@ -0,0 +1,12 @@
+#ifndef PRINTTRENDINGMOVIES_H
+#define PRINTTRENDINGMOVIES_H
+#include <vector>
+
+class Movie;
+
+class printTrendingMovies {
+    public:
+        void printTrendingMovieList(std::vector<Movie> v);
+};
+
+#endif
