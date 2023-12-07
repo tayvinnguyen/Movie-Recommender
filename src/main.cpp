6f0d61bbf868c2000bec59a74ecf23001ed85124
tnguy029 <tnguy029@ucr.edu>
2023-12-07 01:29:42 -0800

    fixed merge issues with main

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include "../header/Movie.h"
 #include "../header/movieParse.h"
 #include "../header/genreSearch.h"
+#include "../header/actorSearch.h"
 
 #include <iostream>
 
@@ -13,6 +14,8 @@ int main() {
     // Example: Use GenreSearch to search for and print movies with a specific genre or subgenre
     GenreSearch genreSearch(movies);
     genreSearch.searchAndPrint("ACTION");
+    ActorSearch actorSearch(movies);
+    actorSearch.searchAndPrint("Leonardo DiCaprio");
 
     return 0;
 }
