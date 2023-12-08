e0b4ea94290644ea686906cd55a58ef6cd270e58
tnguy029 <tnguy029@ucr.edu>
2023-12-08 13:12:33 -0800

    updated rating and trending tests

--- test/RatingAndTrendingTests.cpp
+++ test/RatingAndTrendingTests.cpp
@@ -3,11 +3,13 @@
 #include "../header/CalculateTrendScore.h"
 #include "../header/topTrendMovies.h"
 #include "../header/RatingSort.h"
+#include "gtest/gtest.h"
 using namespace std;
+
 
 TEST(CalcTrendScoreTests, testScore) {
     CalculateTrendScore c;
-    EXPECT_EQ (c.calcTrendScore(2023, 8.7), 40);
+    EXPECT_EQ (c.calcTrendScore(2023, 8.7), 50);
 }
 
 TEST(CalcTrendScoreTest, testScore2) {
@@ -18,7 +20,10 @@ TEST(CalcTrendScoreTest, testScore2) {
 TEST(RatingSortTests, testCompareByScore) {
     Movie m("Spider-Man", 2023, "ANIMATION", "ACTION", "Shameik Moore", "Joaquim Dos Santos", 8.7, 40);
     Movie m2("Transformers", 2015, "ACTION", "ADVENTURE", "Michael Bay", "Michael Bay", 7.5, 8);
-    RatingSort R;
-    bool condition = R.compareByScore(m, m2)
+    vector<Movie> movies;
+    movies.push_back(m);
+    movies.push_back(m2);
+    RatingSort R(movies);
+    bool condition = R.compareByScore(m, m2);
     ASSERT_TRUE(condition);
 }
